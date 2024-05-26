383810744d47f90084d79cb75de6ec62f0a33f8c
Mahavir745 <mahavir777kumar@gmail.com>
2024-05-26 11:44:09 +0530

    new update

--- logStorage.js
+++ logStorage.js
@@ -15,8 +15,8 @@ document.addEventListener('DOMContentLoaded', function() {
     let foundUser = logdata.find(user => user.email === loginUser && user.password === loginPass);
 
     if (foundUser) {
-      newmessage.innerHTML = `Login successful: <a href="view.html">View</a>`;
-      newmessage.style.color = "green";
+      // newmessage.innerHTML = `Login successful: <a href="view.html">View</a>`;
+      // newmessage.style.color = "green";
       
       // Save the current user data to localStorage
       localStorage.setItem('currentUser', JSON.stringify(foundUser));
